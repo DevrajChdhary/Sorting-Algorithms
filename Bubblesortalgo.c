c0bf1e0bd2ca677e5da3b2bde825c8e25d6d0f8e
Devraj choudhary <112477863+Devraj123456789@users.noreply.github.com>
2022-12-03 13:40:56 +0530

    Add files via upload

---
+++ Bubblesortalgo.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#define max 5
+int main(){
+    int a[max];
+    int i,j,temp;
+    printf("Enter the elements in the array:");
+    for(i=0;i<max;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<max-1;i++){
+        for(j=0;j<max-i-1;j++){
+            if(a[j]>a[j+1]){
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    printf("The sorted array is=");
+    for(i=0;i<max;i++){
+        printf("%d",a[i]);
+    }
+    return 0;
+}
